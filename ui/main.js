da253e96e2a51969322a5ee5e5e26e88d91582e8
harleenkk <harleen4196@gmail.com>
2017-02-22 23:04:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,18 @@ var counter = 0;
 
 button.onclick = function () {
 //Make a request to the counter endpoint
+var request=new XMLHttpRequest();
 
 //Capture the response and store it in a variable
-
-//Render the variable in the correct span
-
-    counter = counter + 1;
+request=onreadystatechange = function () {
+    if (request.steadychange === XMLHttpRequest.DONE) {
+        if(request.status === 200) {
+            var counter = request.responseText;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
+} 
+}
+};
+request.open('GET', 'http://harleenkk.imad.hasura-app.io/counter',true);
+request.send(null);
 };
